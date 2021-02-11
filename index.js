0e13affa3d854266648494556ce48c25acc8b7b1
Victoria <vicglee1234007@gmail.com>
2021-02-10 22:22:57 -0600

    Added in 404 & 500 error routes + other middleware

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 const PORT = 5000;
 
-app.use(express.static('public'))
+//Static Middleware
+app.use(express.static(path.join(__dirname, 'public')))
+//Parsing middleware
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/', (req, res)=>{
-  res.send('We got home!');
-} )
+//Handle 404
+app.use((req, res)=>{
+  console.log("Hitting 404")
+  res.status(404).send("PAGE NOT FOUND?!");
+})
+
+//Handle 500
+app.use((err, req, res, next)=>{
+  console.log("Oh no something broke...")
+  console.error(err.stack);
+  res.status(500).send("Internal server error (T^T)");
+})
+
+app.get('/', (req, res, next)=>{
+  try{
+    res.send('We got home!');
+  } catch(error){
+    next(error)
+  }
+})
+
 
 app.listen(PORT, ()=>{
   console.log(`Serving up content on http://localhost:${PORT}`)
